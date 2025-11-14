02bad6f92b08c716f2b9bc7824a6a7989a6e6924
Lemon2210 <thanhdat.chung2210@gmail.com>
2025-11-14 16:32:03 +0700

    Fix: Cap nhat URL backend Render moi

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Lấy địa chỉ API backend của bạn từ log Render
-const API_URL = 'https://website-ban-hang-adjf.onrender.com'; //
+const API_URL = 'https://website-ban-hang-ed1u.onrender.com'; //
 
 // Kiểm tra xem chúng ta đang ở local hay trên mạng
 const baseURL = process.env.NODE_ENV === 'production'
